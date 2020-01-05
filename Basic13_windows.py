61343fc780ee99f4f5b74d420cf7e76258a7e401
apotree <52978976+apotree@users.noreply.github.com>
2020-01-05 12:38:49 +0900

    Create Basic13_windows.py

---
+++ Basic13_windows.py
@@ -0,0 +1,9 @@
+from tkinter import*  # 윈도우 위젯을 지원하는 라이브러리
+
+window = Tk()
+window.title("윈도우 창 연습------")  # 창 타이틀 이름
+window.geometry("400x100") # 400x100 크기의 윈도우창 오픈
+window.resizable(width=False, height=False)
+
+
+window.mainloop()
