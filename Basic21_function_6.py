5b99b1ad8831888e0eaa92308d26bfc7af69bb35
apotree <52978976+apotree@users.noreply.github.com>
2020-01-06 02:17:53 +0900

    Create Basic21_function_6.py

---
+++ Basic21_function_6.py
@@ -0,0 +1,25 @@
+# 원의 넓이 : 반지름 x 반지름 x 3.14
+# 원의 둘레 : 원의 넓이 x 높이
+
+def circleArea(radius):
+    print(id(radius))
+    area = radius * radius * 3.14
+    return area
+
+def circleVolume(radius, height):
+    vol = circleArea(radius) * height
+    return vol
+
+def printCircle(area, vol, radius):
+    print('반지름 %d인 원의 넓이 %.2f ' % (radius, area))
+    print('반지름 %d인 원의 둘레 %.2f ' % (radius, vol))
+
+def main():
+    radius = int(input('반지름 : '))
+    height = int(input('원의 높이 : '))
+    area = circleArea(radius)
+    vol = circleVolume(radius, height)
+    printCircle(area, vol, radius)
+    print('radius : ', id(radius))
+
+main()
