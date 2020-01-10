5956dae4affc919775f5f4b02dc9ee0e0a39e970
apotree <52978976+apotree@users.noreply.github.com>
2020-01-10 20:45:39 +0900

    Add files via upload

---
+++ deque.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+dq = deque('data')
+for node in dq:
+    print(node.upper(), end='')
+print()
+
+dq.append('aaa')
+dq.appendleft('bbb')
+dq.append('ccc')
+print(dq)
+
+print('deque => ', dq.pop())
+print('deque => ', dq.popleft())
+print('deque => ', dq[-1])
+print('deque => ', dq[0])
+print(dq)
+
+print('t' in dq)
+dq.extend('deque')
+print(dq)
+
+dq.extendleft(reversed('python'))
+print(dq)
+
+dq.reverse()
+print(dq)
