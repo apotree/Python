de370f4f56dd5954650fb1b52558c7b4b82315cd
apotree <52978976+apotree@users.noreply.github.com>
2020-01-14 00:02:51 +0900

    Create Basic12_list_2.py

---
+++ Basic12_list_2.py
@@ -0,0 +1,17 @@
+# List 사용법
+
+aa = [0,2,3,4,5,5,6,9]
+bb = [1,2,3,4,5,6]
+
+
+aaSize = len(aa)
+print(aaSize)
+
+bbSize = len(bb)
+print(bbSize)
+
+
+str = []
+for i in range(0, 100):
+    str.append(i)
+    print("str[i]", end="")
